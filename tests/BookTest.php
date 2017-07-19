5a11dcf81456476d5242690b5209b9784535ef24
Brittany Kerr and Nicole Freed <brittany@>
2017-07-19 09:04:19 -0700

    create method and pass test for Book getTitle()

---
+++ tests/BookTest.php
@@ -0,0 +1,27 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Book.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BookTest extends PHPUnit_Framework_TestCase
+    {
+        function testGetTitle()
+        {
+            $title = "The Little Prince";
+            $test_book = new Book($title);
+
+            $result = $test_book->getTitle();
+
+            $this->assertEquals($title, $result);
+        }
+    }
+?>
