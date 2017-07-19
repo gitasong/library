838c218597392bdff97147e95497ee48403c4ca9
Brittany Kerr and Nicole Freed <nicole@>
2017-07-19 10:21:52 -0700

    fix error in app

--- app/app.php
+++ app/app.php
@@ -1,8 +1,7 @@
 <?php
 
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Stylist.php";
-    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Book.php";
 
     $server = 'mysql:host=localhost:8889;dbname=hair_salon';
     $username = 'root';
@@ -16,7 +15,7 @@
 
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
-    
+
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig');
     });
