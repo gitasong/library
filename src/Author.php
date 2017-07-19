cb81ccf6a138e6aac00e4a4d856f49ea81bfaf50
Brittany Kerr and Nicole Freed <brittany@>
2017-07-19 13:14:53 -0700

    write function get author name

---
+++ src/Author.php
@@ -0,0 +1,96 @@
+<?php
+    class Author
+    {
+        private $author_name;
+        private $id;
+
+        function __construct($author_name, $id = null)
+        {
+            $this->author_name = $author_name;
+            $this->id = $id;
+        }
+
+        function getAuthorName()
+        {
+            return $this->author_name;
+        }
+
+        // function setAuthorName($new_author_name)
+        // {
+        //     $this->author_name = (string) $new_author_name;
+        // }
+        //
+        // function save()
+        // {
+        //     $executed = $GLOBALS['DB']->exec("INSERT INTO authors (author_name) VALUES ('{$this->getAuthorName()}')");
+        //     if ($executed) {
+        //         $this->id = $GLOBALS['DB']->lastInsertId();
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }
+        // }
+        //
+        // function getId()
+        // {
+        //     return $this->id;
+        // }
+        //
+        // static function getAll()
+        // {
+        //     $returned_authors = $GLOBALS['DB']->query("SELECT * FROM authors");
+        //     $authors = array();
+        //     foreach($returned_authors as $author) {
+        //         $author_author_name = $author['author_name'];
+        //         $author_id = $author['id'];
+        //         $new_author = new Author($author_author_name, $author_id);
+        //         array_push($authors, $new_author);
+        //     }
+        //     return $authors;
+        // }
+        //
+        // static function deleteAll()
+        // {
+        //     $GLOBALS['DB']->exec("DELETE FROM authors;");
+        // }
+        //
+        // static function find($search_id)
+        // {
+        //     $returned_authors = $GLOBALS['DB']->prepare("SELECT * FROM authors WHERE id = :id");
+        //     $returned_authors->bindParam(':id', $search_id, PDO::PARAM_STR);
+        //     $returned_authors->execute();
+        //     foreach ($returned_authors as $author) {
+        //         $author_author_name = $author['author_name'];
+        //         $author_id = $author['id'];
+        //         if ($author_id == $search_id) {
+        //             $returned_author = new Author($author_author_name, $author_id);
+        //         }
+        //     }
+        //     return $returned_author;
+        // }
+        //
+        // function updateAuthorName($new_author_name)
+        // {
+        //     $executed = $GLOBALS['DB']->exec("UPDATE authors SET author_name = '{$new_author_name}' WHERE id = {$this->getId()};");
+        //     if ($executed) {
+        //         $this->setAuthorName($new_author_name);
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }
+        // }
+        //
+        // function delete()
+        // {
+        //     $executed = $GLOBALS['DB']->exec("DELETE FROM authors WHERE id = {$this->getId()};");
+        //     if ($executed) {
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }
+        // }
+    }
+
+
+
+?>
---
+++ tests/AuthorTest.php
@@ -0,0 +1,139 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Author.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class AuthorTest extends PHPUnit_Framework_TestCase
+    {
+        // protected function tearDown()
+        // {
+        //     Author::deleteAll();
+        // }
+
+        function testGetAuthorName()
+        {
+            $author_name = "The Little Prince";
+            $test_author = new Author($author_name);
+
+            $result = $test_author->getAuthorName();
+
+            $this->assertEquals($author_name, $result);
+        }
+
+        // function testSetAuthorName()
+        // {
+        //     $author_name = "The Little Prince";
+        //     $test_author = new Author($author_name);
+        //     $new_author_name = "The Author of Laughter and Forgetting";
+        //
+        //     $test_author->setAuthorName($new_author_name);
+        //     $result = $test_author->getAuthorName();
+        //
+        //     $this->assertEquals($new_author_name, $result);
+        // }
+        //
+        // function testSave()
+        // {
+        //     $author_name = "The Unbearable Lightness of Being";
+        //     $test_author = new Author($author_name);
+        //
+        //     $executed = $test_author->save();
+        //
+        //     $this->assertTrue($executed, "Author not successfully saved to database");
+        // }
+        //
+        // function testGetId()
+        // {
+        //     $author_name = "Clifford";
+        //     $test_author = new Author($author_name);
+        //     $test_author->save();
+        //
+        //     $result = $test_author->getId();
+        //
+        //     $this->assertEquals(true, is_numeric($result));
+        //
+        // }
+        //
+        // function testGetAll()
+        // {
+        //     $author_name_1 = "Secrets of the Talking Jaguar";
+        //     $test_author_1 = new Author($author_name_1);
+        //     $test_author_1->save();
+        //
+        //     $author_name_2 = "The Smell of Rain on Dust";
+        //     $test_author_2 = new Author($author_name_2);
+        //     $test_author_2->save();
+        //
+        //     $result = Author::getAll();
+        //
+        //     $this->assertEquals([$test_author_1, $test_author_2], $result);
+        // }
+        //
+        // function testDeleteAll()
+        // {
+        //     $author_name_1 = "Secrets of the Talking Jaguar";
+        //     $test_author_1 = new Author($author_name_1);
+        //     $test_author_1->save();
+        //
+        //     $author_name_2 = "The Smell of Rain on Dust";
+        //     $test_author_2 = new Author($author_name_2);
+        //     $test_author_2->save();
+        //
+        //     Author::deleteAll();
+        //     $result = Author::getAll();
+        //
+        //     $this->assertEquals([], $result);
+        // }
+        //
+        // function testFind()
+        // {
+        //     $author_name_1 = "Secrets of the Talking Jaguar";
+        //     $test_author_1 = new Author($author_name_1);
+        //     $test_author_1->save();
+        //
+        //     $author_name_2 = "The Smell of Rain on Dust";
+        //     $test_author_2 = new Author($author_name_2);
+        //     $test_author_2->save();
+        //
+        //     $result = Author::find($test_author_2->getId());
+        //
+        //     $this->assertEquals($test_author_2, $result);
+        // }
+        //
+        // function testUpdateAuthorName()
+        // {
+        //     $author_name = "The Little Prince";
+        //     $test_author = new Author($author_name);
+        //     $test_author->save();
+        //     $new_author_name = "The Author of Laughter and Forgetting";
+        //
+        //     $test_author->updateAuthorName($new_author_name);
+        //
+        //     $this->assertEquals("The Author of Laughter and Forgetting", $test_author->getAuthorName());
+        // }
+        //
+        // function testDelete()
+        // {
+        //     $author_name_1 = "Secrets of the Talking Jaguar";
+        //     $test_author_1 = new Author($author_name_1);
+        //     $test_author_1->save();
+        //
+        //     $author_name_2 = "The Smell of Rain on Dust";
+        //     $test_author_2 = new Author($author_name_2);
+        //     $test_author_2->save();
+        //
+        //     $test_author_1->delete();
+        //
+        //     $this->assertEquals([$test_author_2], Author::getAll());
+        // }
+    }
+?>
