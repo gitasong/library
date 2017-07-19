5a699349b9c80b73385a5d3cd7046e8779c894c0
Brittany Kerr and Nicole Freed <nicole@>
2017-07-19 10:10:09 -0700

    add boilerplate to index.php and app.php with root route (/)

---
+++ app/app.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+    
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+?>
